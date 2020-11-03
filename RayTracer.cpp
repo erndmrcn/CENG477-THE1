bad3e5aaef69f7d530a020d286774cf3489d7c60
erndmrcn <64655601+erndmrcn@users.noreply.github.com>
2020-11-03 16:44:10 +0300

    Update RayTracer.cpp

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -1,281 +1,212 @@
 #include <iostream>
-#include <stdlib.h>
-#include <math.h>
-#include <time.h>
-#include <string.h>
-#include <fstream>
+#include "parser.h"
+#include "ppm.h"
 
 #define ABS(a) ((a)>0?(a):-1*(a))
+#define EPSILON 0.000000001
 
+typedef unsigned char RGB[3];
 using namespace std;
+
 class Vec3
 {
-private:
-	double x,y,z;
-public:
-	// here comes methods
-	double getX()
-	{
-		return x;
-	}
-	void setX(double a)
-	{
-		x = a;
-	}
-	double getY()
-	{
-		return y;
-	}
-	void setY(double b)
-	{
-		y = b;
-	}
-	double getZ(void)
-	{
-		return z;
-	}
-	void setZ(double c)
-	{
-		z = c;
-	}
-	double len(void)
-	{
-		return sqrt(x^2+y^2+z^2);
-	}
-	double len2(void)
-	{
-		return x^2+y^2+z^2;
-	}
-
-}
-
-class Color
-{
-private:
-	int r,g,b;
-public:
-	/* here comes methods */
-	int getRed(void)
-	{
-		return r;
-	}
-	void setRed(int c)
-	{
-		r = c;
-	}
-	int getGreen(void)
-	{
-		return g;
-	}
-	void setGreen(int c)
-	{
-		g = c;
-	}
-	int getBlue(void)
-	{
-		return b;
-	}
-	void setBlue(int c)
-	{
-		b = c;
-	}
+    public:
+        double x, y, z;
 }
 
 class Ray
 {
-private:
-	Vec3 a;
-	Vec3 b;
+    // represented like r(t) = e + dt 
 public:
-	// for methods 
-	Vec3 getV1(void)
-	{
-		return a;
-	}
-	Vec3 getV1(void)
-	{
-		return b;
-	}
-	/* add set functions */
-}
+    // coordinates of the first vector (e)
+    Vec3 a;
+    // coordinates of the second vector (d)
+    Vec3 b;
+};
 
-class Sphere
+// Cross product of 2 vectors
+// i*i = 0(i), i*j = 1(k), i*k = 1(j)
+// j*j = 0(j), j*i = -1(k), j*k = 1(i)
+// k*k = 0(k), k*i = -1(j), k*j = -1(i)
+// x -> i*i + j*k + k*j
+// y -> j*j + i*k + k*i
+// z -> k*k + i*j + j*i
+Vec3 crossProduct(Vec3 a, Vec3 b)
 {
-private:
-	Vec3 center;
-	double r; // radius
-	Color color;
-public:
-	//methods
-	Vec3 getCenter(void)
-	{
-		return center;
-	}
-	double getRadius(void)
-	{
-		return r;
-	}
-	Color getColor(void)
-	{
-		return color;
-	}
+    Vec3 tmp;
+
+    tmp.x = a.y*b.z - a.z*b.y;
+    tmp.y = a.z*b.x - a.x*b.z;
+    tmp.z = a.x*b.y - a.y*b.x;
+
+    return tmp;
 }
 
-class Triangle
+// Dot product of 2 vectors
+// i*i = 1, i*j = 0, i*k = 0
+double dotProduct(Vec3 a, Vec b)
 {
-private:
-	Vec3 a,b,c;
-	Color color;
-public:
-	/* methods */
-	Vec3 getFirst(void)
-	{
-		return a;
-	}
-	Vec3 getSecond(void)
-	{
-		return b;
-	}
-	Vec3 getThird(void)
-	{
-		return c;
-	}
-	Color getColor(void)
-	{
-		return color;
-	}
+    return a.x*b.x+a.y*b.y+a.z*b.z;
 }
 
-int numSpheres;
-int numTriangles;
-
-Sphere* spheres;
-Triangle* triangles;
-
-class Camera
+// length square function
+double length2(Vec3 a)
 {
-private:
-	Vec3 pos;
-	Vec3 gaze;
-	Vec3 v;
-	Vec3 u;
-	double l,r,b,t;
-	double di;
-public:
-	/* methods */
-	Vec3 getPos(void)
-	{
-		return pos;
-	}
-	Vec3 getGaze(void)
-	{
-		return gaze;
-	}
-	Vec3 getU(void)
-	{
-		return u;
-	}
-	Vec3 getV(void)
-	{
-		return v;
-	}
-
-	double getLeft(void)
-	{
-		return l;
-	}
-	double getRight(void)
-	{
-		return r;
-	}
-	double getTop(void)
-	{
-		return t;
-	}
-      	double getBottom(void)
-	{
-		return b;
-	}
-	double getDi(void)
-	{
-		return di;
-	}
-	/* write set functions for private data */
+    return (a.x*a.x+a.y*a.y+a.z*a.z);
 }
 
-Camera cam;
-
-int sizeX, sizeY;
-
-double pixelW, pixelH;
-
-double halfPixelW, halfPixelH;
-
-
-Color **image;
-
-Vec3 cross(Vec3 a, Vec3 b)
+// length function
+double length(Vec3 a)
 {
-	Vec3 tmp;
-
-	tmp.setX(a.getY()*b.getZ() - b.getY()*a.getX());
-        tmp.setY(b.getX()*a.getZ() - a.getX()*b.getZ());
-	tmp.setZ(a.getX()*b.getY() - b.getX()*a.getY());
-
-	return tmp;	
+    return sqrt(a.x*a.x+a.y*a.y*+a.z*a.z)
 }
 
-double dot(Vec3 a, Vec3 b)
-{
-	return a.getX()*b.getX() + a.getY()*b.getY() + a.getZ()*b.getZ();
-}
-
+// normalize function
 Vec3 normalize(Vec3 v)
 {
-	Vec3 tmp;
-	double d;
+    Vec3 tmp;
+    double l;
 
-	d = v.len();
+    l = length(v);
+    tmp.x = v.x/l;
+    tmp.y = v.y/l;
+    tmp.z = v.z/l;
 
-	tmp.setX(v.getX()/d);
-	tmp.setY(v.getY()/d);
-	tmp.setZ(v.getZ()/d);
+    return tmp;
 }
 
-//
-// add,mult,distance,equal methods into the Vec3 class
-//
-
-/* file operations read scene and camera files */
-
-void readScene(Sring filename)
+// add function
+Vec3 add(Vec3 a, Vec3 b)
 {
+    Vec3 tmp;
+    tmp.x = a.x+b.x;
+    tmp.y = a.y+b.y;
+    tmp.z = a.z+b.z;
 
+    return tmp;
 }
 
-void readCamera(string filename)
+// multiplication of a matrix by a scalar
+Vec3 mult(Vec3 v, double d)
 {
-	double gazex,gazey,gazez,posx,posy,posz,right,left,bottom,top,dimension,height,width;
-	Vec3 v1,v2,v3;
-	ifstream myfile open(filename, ios:in);
-	myfile.open("simple_camera.txt");
-	myfile >> posx >> posy >> posz;
-	myfile >> gazex >> gazey >> gazez;
-	myfile >> left >> right >> bottom >> top >> dimension >> width >> height;
+    Vec3 tmp;
+    tmp.x = v.x*d;
+    tmp.y = v.y*d;
+    tmp.z = v.z*d;
 
-	// continue from here //
-
+    return tmp;
 }
 
-int main(void) // for now void 
+// distance between two vectors
+double distance(Vec3 a, Vec3 b)
 {
-	// take input
-	// read files
-	// start image 2D grid
-	// generate rays
-	// check for intersects
-	// write 2D grid
+    return sqrt((a.x-b.x)*(a.x-b.x) + (a.y-b.y)*(a.y-b.y) + (a.z-b.z)*(a.z-b.z));
+}
 
-	// write an image file
-	
+// check if two vectors is equal
+int equal(Vec3 a, Vec3 b)
+{
+    if((ABS((a.x-b.x))<EPSILON) && (ABS((a.y-b.y))<EPSILON) && (ABS((a.z-b.z))<EPSILON))
+        return 1;
+    else 
+        return 0;
+}
+
+// ray generation function 
+Ray generateRay(int i, int j)
+{
+    // ray representation -> r(t) = o + td
+    // o-> origin, t->variable
+    Ray tmp;
+    // su = (i + 0.5)(r-l)/nx 
+    // r->right, l->left, nx->image width
+    // sv = (i + 0.5)(t-b)/ny
+    // t->top, b->botton, ny->image height
+    // m = e + -w(distance) --- w direction vector
+    // q = m + lu + tv --- u&v are direction vectors
+    // s = q + (su)u -(sv)v --- u&v are direction vectors
+
+    Vec3 su, sv, s;
+
+    // we need each camera's position, pixelwidth
+    // pixelHeight and so on
+
+    // for each camera different rays will be created
+    // ...
+}
+
+// interseciton of sphere, returns t value
+double intersectSphere(Ray r, Sphere r)
+{
+    double A, B, C; // constants for the quadratic equation
+
+    double delta; // solving for quadratic eqn.
+    Vec3 scenter;
+    double sradius;
+    Vec3 p;
+
+    double t, t1, t2;
+    int i;
+
+    scenter = s.center;
+    sradius = s.r;
+
+    // calculate A B C and solve for t
+    // return t
+}
+
+int main(int argc, char* argv[])
+{
+    // Sample usage for reading an XML scene file
+    parser::Scene scene;
+    scene.loadFromXml(argv[1]);
+    /****
+     **** 
+            mesh -> scene.meshes -> vector
+            faces -> scene.meshes[i].faces -> vector
+            vertex_id -> scene.meshes[i].faces[j].v0_id -> vertex0 of ith mesh jth faces -> int
+            vertex_data -> scene.vertex_data[scene.meshes[i].faces[j].v0_id].x
+                                                                            .y
+                                                                            .z -> we can reach each vertex coordinate
+     ****
+    ****/
+
+    // The code below creates a test pattern and writes
+    // it to a PPM file to demonstrate the usage of the
+    // ppm_write function.
+    //
+    // Normally, you would be running your ray tracing
+    // code here to produce the desired image.
+
+    const RGB BAR_COLOR[8] =
+    {
+        { 255, 255, 255 },  // 100% White
+        { 255, 255,   0 },  // Yellow
+        {   0, 255, 255 },  // Cyan
+        {   0, 255,   0 },  // Green
+        { 255,   0, 255 },  // Magenta
+        { 255,   0,   0 },  // Red
+        {   0,   0, 255 },  // Blue
+        {   0,   0,   0 },  // Black
+    };
+
+    int width = 640, height = 480;
+    int columnWidth = width / 8;
+
+    unsigned char* image = new unsigned char [width * height * 3];
+
+    int i = 0;
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            int colIdx = x / columnWidth;
+            image[i++] = BAR_COLOR[colIdx][0];
+            image[i++] = BAR_COLOR[colIdx][1];
+            image[i++] = BAR_COLOR[colIdx][2];
+        }
+    }
+
+    write_ppm("test.ppm", image, width, height);
+
 }
